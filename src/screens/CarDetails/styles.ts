48eb469d827227c19372fef2696357479914e31f
Caroline Vieira <carolinesanvieira@gmail.com>
2022-01-15 00:34:56 +0100

    Details Car

--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,4 +1,7 @@
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import styled from "styled-components/native";
+import { ScrollView } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
@@ -10,4 +13,68 @@ export const Header = styled.View`
 
   justify-content: space-between;
   align-items: center;
+
+  position: absolute;
+  margin-top: ${getStatusBarHeight() + 18}px;
+  margin-left: 24px;
+`;
+export const CarImages = styled.View`
+  margin-top: ${getStatusBarHeight() + 32}px;
+`;
+export const Content = styled(ScrollView).attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: "center",
+  },
+  showsVerticalScrollIndicator: false,
+})``;
+export const Details = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secundary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_details};
+
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secundary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secundary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_details};
+
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secundary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text};
+  line-height: ${RFValue(25)}px;
+
+  text-align: justify;
+`;
+export const Accessories = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
