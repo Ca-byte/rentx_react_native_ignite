878d8c2dd11d3c29388f4f5055aa437bf8cc257e
Caroline Vieira <carolinesanvieira@gmail.com>
2022-01-18 23:52:21 +0100

    Scheduling Screen

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
 
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 
-import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 import theme from "./src/styles/theme";
 
 export default function App() {
@@ -27,7 +27,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValueContainer,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+        <Title>
+          Escolha uma {"\n"}
+          data de início e {"\n"}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>De</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue>12/02/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>Até</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue>12/02/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+      <Content>
+        <Calendar />
+      </Content>
+      <Footer>
+        <Button title="Confimar" />
+      </Footer>
+    </Container>
+  );
+}
