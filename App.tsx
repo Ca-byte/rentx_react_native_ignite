970c83cec899203546568f083000b36a1c360a7a
Caroline Vieira <carolinesanvieira@gmail.com>
2022-01-20 23:25:09 +0100

    Scheduling Complete

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
 
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 
-import { SchedulingDetails } from "./src/screens/SchedulingDetails";
+import { SchedulingComplete } from "./src/screens/SchedulingComplete";
 import theme from "./src/styles/theme";
 
 export default function App() {
@@ -27,7 +27,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
---
+++ src/screens/SchedulingComplete/styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.header};
+  padding-top: 86px;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secundary_600};
+  font-size: ${RFValue(30)}px;
+
+  margin-top: 20px;
+`;
+
+export const Message = styled.Text`
+  color: ${({ theme }) => theme.colors.text_details};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  line-height: ${RFValue(25)}px;
+  text-align: center;
+
+  margin-top: 12px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  align-items: center;
+
+  margin: 30px 0;
+`;
