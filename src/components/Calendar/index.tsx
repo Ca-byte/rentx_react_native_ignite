7d26ca203b37509f8b6e7b763c194403fd2a2f99
Caroline Vieira <carolinesanvieira@gmail.com>
2022-04-03 21:11:30 +0200

    Calendar Interval

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,56 +1,37 @@
 import React from "react";
 import { useTheme } from "styled-components/native";
-
 import { Feather } from "@expo/vector-icons";
+import { generateInterval } from "./generateInterval";
+
+import { ptBr } from "./localeConfig";
 
 import {
   Calendar as CustomCalendar,
   LocaleConfig,
+  CalendarProps,
 } from "react-native-calendars";
 
-LocaleConfig.locales["pt-br"] = {
-  monthNames: [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ],
-  monthNamesShort: [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
-  ],
-  dayNames: [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
-  ],
-  dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"],
-};
+LocaleConfig.locales["pt-br"] = ptBr;
 LocaleConfig.defaultLocale = "pt-br";
 
-export function Calendar() {
+interface MarkedDateProps {
+  [date: string]: {
+    color: string;
+    textColor: string;
+    disabled?: boolean;
+    disableTouchEvent?: boolean;
+  };
+}
+
+interface DayProps {
+  dateString: string;
+  day: number;
+  month: number;
+  year: number;
+  timestamp: number;
+}
+
+function Calendar({ markedDates, onDayPress }: CalendarProps) {
   const theme = useTheme();
   return (
     <CustomCalendar
@@ -80,7 +61,11 @@ export function Calendar() {
         },
       }}
       firstDay={1}
-      minDate={"2022-01-18"}
-    ></CustomCalendar>
+      minDate={String(new Date())}
+      markingType="period"
+      markedDates={markedDates}
+      onDayPress={onDayPress}
+    />
   );
 }
+export { Calendar, MarkedDateProps, DayProps, generateInterval };
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "react-native";
 import { useTheme } from "styled-components";
 import { BackButton } from "../../components/BackButton";
 import { Button } from "../../components/Button";
-import { Calendar } from "../../components/Calendar";
+import {
+  Calendar,
+  DayProps,
+  generateInterval,
+  MarkedDateProps,
+} from "../../components/Calendar";
 
 import ArrowSvg from "../../assets/arrow.svg";
 
@@ -22,6 +27,12 @@ import {
 import { useNavigation } from "@react-navigation/native";
 
 export function Scheduling() {
+  const [markedDates, setMarkedDates] = useState<MarkedDateProps>(
+    {} as MarkedDateProps
+  );
+  const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>(
+    {} as DayProps
+  );
   const navigation = useNavigation<any>();
   const theme = useTheme();
 
@@ -31,6 +42,20 @@ export function Scheduling() {
 
   function handleBack() {
     navigation.goBack();
+  }
+
+  function handleChangeDate(date: DayProps) {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+
+    if (start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+
+    setLastSelectedDate(end);
+    const interval = generateInterval(start, end);
+    setMarkedDates(interval);
   }
 
   return (
@@ -67,7 +92,7 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
       <Content>
-        <Calendar />
+        <Calendar markedDates={markedDates} onDayPress={handleChangeDate} />
       </Content>
       <Footer>
         <Button title="Confimar" onPress={handleConfirmScheduling} />
