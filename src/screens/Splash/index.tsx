c9ad406710a8c76bc537673109ab6a0dd01b6834
Caroline Vieira <carolinesanvieira@gmail.com>
2022-06-04 14:08:22 +0200

    animation - reanimated

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Dimensions } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
 
 import { Container } from "./styles";
+
+const WIDTH = Dimensions.get("window").width;
 
 export function Splash() {
   const animation = useSharedValue(0);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateY: animation.value }],
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 3000,
+            easing: Easing.elastic(5),
+          }),
+        },
+      ],
     };
   });
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 75;
+    animation.value = Math.random() * (WIDTH - 100);
   }
   return (
     <Container>
